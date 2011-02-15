c66379502662ef83c809d1a758ffaeccd4f00939
Greg Sochanik <greg.sochanik@7digital.com>
2011-02-15 10:01:06 +0000

    Added Price stuff

--- src/HtmlReaderTest/HtmlReaders/BritishHorseRacingWebReaderTest.cs
+++ src/HtmlReaderTest/HtmlReaders/BritishHorseRacingWebReaderTest.cs
@@ -63,8 +63,8 @@ namespace HtmlReaderTest.HtmlReaders
 						.SelectNodes("tr").Skip(2);
 
 					_currentCourse = _selector.SelectNodes("b").ElementAt(1).InnerHtml;
-					// from results build ResultList
 
+					// from results build ResultList))
 					foreach (var result in results)
 					{
 						yield return ParseResult(result);
@@ -87,13 +87,49 @@ namespace HtmlReaderTest.HtmlReaders
 			IEnumerable<HtmlNode> allResults = doc.DocumentNode.QuerySelectorAll("td");
 			string position = allResults.ElementAt(0).QuerySelector("b").InnerText;
 			string name = allResults.ElementAt(1).QuerySelector("b").InnerText;
+			string jockey = allResults.ElementAt(2).QuerySelector("b").InnerText;
+			string startingPrice = allResults.ElementAt(3).QuerySelector("b").InnerText.Trim();
+			var price = new Price{DecimalPrice = 0,Denominator=0,Numerator = 0};
+			try {
+				price = new Price(startingPrice);
+			} catch(ArgumentException) {}
+
 			return new Result
 			       	{
 						CourseName = courseName,
 						EventName = eventName,
-						Position = Convert.ToInt32(position),
-						HorseName = name
+						Position = position,
+						HorseName = name,
+						JockeyName = jockey,
+						StartingPriceDecimal = price.DecimalPrice,
+						StartingPriceDenominator = price.Denominator,
+						StartingPriceNumerator = price.Numerator
 					};
+		}
+
+	}
+
+	public class Price
+	{
+		public int Numerator { get; set; }
+		public int Denominator { get; set; }
+		public decimal DecimalPrice { get; set; }
+
+		public Price() {}
+		public Price(string formattedPrice) {
+			var strings = formattedPrice.Split(new[] {" - "}, StringSplitOptions.None);
+			if(strings.Length < 2)
+				throw new ArgumentException("odds must be in the \"n / d\" format (e.g. 100/1)");
+
+			Numerator = int.Parse(strings[0]);
+			Denominator = int.Parse(strings[1]);
+
+			if(Numerator > 0 && Denominator > 0)
+				DecimalPrice = 1 + ((decimal)Numerator/Denominator);
+		}
+
+		public override string ToString() {
+			return string.Format("{0} - {1}", Numerator, Denominator);
 		}
 	}
 
@@ -104,13 +140,13 @@ namespace HtmlReaderTest.HtmlReaders
         public string EventName { get; set; }
         public string Going { get; set; }
         public string Weather { get; set; }
-        public int Position { get; set; }
+        public string Position { get; set; }
         public string HorseName { get; set; }
         public string TrainerName { get; set; }
         public string JockeyName { get; set; }
         public string Weight { get; set; }
-        public int StartingPriceNumerator { get; set; } 
-        public int StartingPriceDenominator { get; set; } // eg - at the moment its A/B or A-B 3/1
+        public int StartingPriceNumerator { get; set; } // eg - the 1 in 1/2
+        public int StartingPriceDenominator { get; set; } // eg - the 2 in 1/2
         public decimal StartingPriceDecimal { get; set; } // eg 2-1 = 3.0, 1-3 on = 1.33 etc
         public string BeatenDistance { get; set; }
 	}
